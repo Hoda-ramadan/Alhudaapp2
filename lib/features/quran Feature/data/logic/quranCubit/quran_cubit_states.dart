6860d86057ce65a73fbf0fe7c45d4d573dfffa0a
Parawita <kamalahmedkaml68@gmail.com>
2025-10-22 13:18:44 +0300

    add bloc package and make quran States

---
+++ lib/features/quran Feature/data/logic/quranCubit/quran_cubit_states.dart
@@ -0,0 +1,19 @@
+import 'package:alhuda/features/quran%20Feature/data/model/quran_model/quran_model.dart';
+
+class QuranStates {}
+
+class QuranIntialState extends QuranStates {}
+
+class QuranLodingState extends QuranStates {}
+
+class QuranSucssesState extends QuranStates {
+  final QuranModel quran;
+
+  QuranSucssesState({required this.quran}); 
+}
+
+class QuranFailState extends QuranStates {
+  final String errorMessage;
+
+  QuranFailState({required this.errorMessage});
+}
