726ed34b1b0be8317bf6c53135fc0aee15ae8d30
Huda Ramadan <011hodaramadan@gmail.com>
2025-11-03 06:55:02 -0800

    adhan logic

--- lib/features/quran Feature/data/logic/Adhan_logic/Adhan_states.dart
+++ lib/features/quran Feature/data/logic/Adhan_logic/Adhan_states.dart
@@ -1,1 +1,19 @@
+import 'package:alhuda/models/adhan_model/adhan_model.dart';
 
+class AdhanStates {}
+
+class AdhanIntialState extends AdhanStates {}
+
+class AdhanLodingState extends AdhanStates {}
+
+class AdhanSucssesState extends AdhanStates {
+  final AdhanModel adhanModel;
+
+  AdhanSucssesState({required this.adhanModel});
+}
+
+class AdhanFailState extends AdhanStates {
+  final String errorMessage;
+
+  AdhanFailState({required this.errorMessage});
+}
